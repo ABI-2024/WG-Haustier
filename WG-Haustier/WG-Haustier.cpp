fabf68a6e547fb40673b8075758256a05f902205
Aeneas Schwaiger <schwaiger@max-eyth-schule.de>
2022-10-13 15:41:32 +0200

    Ausgabe auf Fabio gesetzt

--- WG-Haustier/WG-Haustier.cpp
+++ WG-Haustier/WG-Haustier.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello Fabio!\n";
+
 }
 
 // Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
